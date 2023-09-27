3ea2e24e006fee3fad6fef12c05fc054d8acbd0c
Guyo <guyo.halake@students.moringaschool.com>
2023-09-27 15:05:40 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// Divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Square arrow function
+  const square = (x) => x * x;
+  
+  // Add arrow function
+  const add = (x, y) => x + y;
+  
+  // Export the functions
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
